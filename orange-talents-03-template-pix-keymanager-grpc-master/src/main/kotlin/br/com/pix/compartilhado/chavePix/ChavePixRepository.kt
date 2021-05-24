f82ae5571bf8fb69039b6a0080f107f64687c5d4
Jrenatomt <jrenatomt@gmail.com>
2021-05-24 15:46:51 -0300

    listagem de chaves pix

--- orange-talents-03-template-pix-keymanager-grpc-master/src/main/kotlin/br/com/pix/compartilhado/chavePix/ChavePixRepository.kt
+++ orange-talents-03-template-pix-keymanager-grpc-master/src/main/kotlin/br/com/pix/compartilhado/chavePix/ChavePixRepository.kt
@@ -7,9 +7,13 @@ import java.util.*
 @Repository
 interface ChavePixRepository : JpaRepository<ChavePix, UUID> {
 
+    fun existsByIdCliente(idCliente: UUID): Boolean
+
     fun existsByChave(chave: String): Boolean
 
     fun findByIdAndIdCliente(id: UUID, idCliente: UUID): ChavePix?
 
     fun findByChave(chave: String): Optional<ChavePix>
+
+    fun findAllByIdCliente(idCliente: UUID): List<ChavePix>
 }
---
+++ orange-talents-03-template-pix-keymanager-grpc-master/src/main/kotlin/br/com/pix/consultaPix/ListaChavesGrpcEndpoint.kt
@@ -0,0 +1,50 @@
+package br.com.pix.consultaPix
+
+import br.com.pix.*
+import br.com.pix.compartilhado.chavePix.ChavePixRepository
+import br.com.pix.compartilhado.exception.ClienteNaoEncontradoException
+import com.google.protobuf.Timestamp
+import io.grpc.stub.StreamObserver
+import java.time.ZoneId
+import java.util.*
+import javax.inject.Singleton
+
+@Singleton
+class ListaChavesGrpcEndpoint(private val repository: ChavePixRepository) :
+    KeyManagerListaServiceGrpc.KeyManagerListaServiceImplBase() {
+
+    override fun listaChaves(request: ListaChavesRequest, responseObserver: StreamObserver<ListaChavesResponse>) {
+
+        if (request.idCliente.isNullOrBlank())
+            throw IllegalArgumentException("Id cliente não pode ser nulo ou vazio!")
+
+        val idCliente = UUID.fromString(request.idCliente)
+
+        if (!repository.existsByIdCliente(idCliente))
+            throw ClienteNaoEncontradoException("Cliente não encontrado!")
+
+        val listaChavesPix = repository.findAllByIdCliente(idCliente).map {
+            ListaChavesResponse.ChaveResponse.newBuilder()
+                .setIdPix(it.id.toString())
+                .setTipoChave(TipoChave.valueOf(it.tipoChavePix.name))
+                .setChave(it.chave)
+                .setTipoConta(TipoConta.valueOf(it.tipoConta.name))
+                .setCriadoEm(it.criadoEm.let { criadoEm ->
+                    val instantCriadoEm = criadoEm.atZone(ZoneId.of("UTC")).toInstant()
+                    Timestamp.newBuilder()
+                        .setSeconds(instantCriadoEm.epochSecond)
+                        .setNanos(instantCriadoEm.nano)
+                        .build()
+                })
+                .build()
+        }
+
+        responseObserver.onNext(ListaChavesResponse.newBuilder()
+            .setIdCliente(idCliente.toString())
+            .addAllChavesPix(listaChavesPix)
+            .build()
+        )
+
+        responseObserver.onCompleted()
+    }
+}
